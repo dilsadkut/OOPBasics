e59d50b1b664549c7b2c45584b1b897c8c11589d
Dılşad Kut <61744549+dilsadkut@users.noreply.github.com>
2021-01-24 10:01:45 +0300

    Added OOPBasics2

---
+++ OOPBasics2/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBasics2
+{
+    //İş sınıfları // business classes 
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.MusteriNo + " müşteri numarasına sahip müşteri eklendi.");
+
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
