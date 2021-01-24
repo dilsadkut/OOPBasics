4e50ce25b5f0260eee8911197b31ce988083c0a6
Dılşad Kut <61744549+dilsadkut@users.noreply.github.com>
2021-01-24 10:20:47 +0300

    Added OOPBasics3

---
+++ OOPBasics3/TasitKrediManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBasics3
+{
+    class TasitKrediManager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+           
+            //
+            //
+            Console.WriteLine("Taşıt kredisi ödeme planı hesaplandı");
+        }
+    }
+}
