b87c8c96b408368e14cef8da26dafb1901786670
Dılşad Kut <61744549+dilsadkut@users.noreply.github.com>
2021-01-28 11:03:50 +0300

    Added GameProject

---
+++ GameProject/CampaignManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class CampaignManager : ICampaignService
+    {
+       
+
+        public void Add(Campaign campaign)
+        {
+            Console.WriteLine("Kampanyalı ürün eklendi. Eklenen kampanyalı ürün:\n"+ campaign.CampaignName);
+        }
+
+       
+        public void Delete(Campaign campaign)
+        {
+            Console.WriteLine("Kampanyalı ürün silindi. Silinen kampanyalı ürün:\n"+ campaign.CampaignName);
+        }
+
+        
+        public void Update(Campaign campaign)
+        {
+            Console.WriteLine("Kampanyalı ürün güncellendi. Güncellenen kampanyalı ürün:\n"+campaign.CampaignName);
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1984,
+                FirstName = "ENGİN",
+                LastName = "DEMİROĞ",
+                IdentityNumber = 12345
+            });
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            CampaignManager campaignManager = new CampaignManager();
+            campaignManager.Add(new Campaign 
+            {
+                CampaignName = "X ürününde %20 indirim"
+            });
+
+           
+
+        }
+    }
+}
